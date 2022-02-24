2321e316aff6bd46ba9f4f5d5c8b424160ca711f
jaehyuklee <>
2022-02-24 14:53:38 +0900

    test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost/api',
+                target: 'http://localhost:3000/api',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
